048c62f48662194e30a9beec64f7a46ed77a2b49
andil <andildine2@gmail.com>
2023-02-02 12:58:13 +0100

    avancée dans les tesst

--- src/Plush.hpp
+++ src/Plush.hpp
@@ -1,1 +1,25 @@
 #pragma once
+
+
+class Plush {
+    public:
+    
+    Plush (const int cost) {
+        _cost = cost;
+
+    }
+
+    Plush(){}
+
+    int get_cost() const {
+        return _cost;
+    }
+
+    void set_cost(const int cost){
+        _cost = cost;
+    }
+
+
+    private:
+        int _cost = 10;
+};
--- src/PlushStore.hpp
+++ src/PlushStore.hpp
@@ -1,1 +1,64 @@
 #pragma once
+#include <string>
+#include <vector>
+#include "Plush.hpp"
+
+class PlushStore {
+    public:
+    
+        PlushStore (const std::string name) {
+            _name = name;
+        }
+
+        PlushStore(){}
+
+        std::string get_name() const {
+            return _name;
+        }
+
+        void set_name(const std::string name){
+            _name = name;
+        }
+
+        double get_wealth_amount() const {
+            return _loan;
+        }
+        int get_stock_size() const {
+            return _vecPlush.size();
+        }
+
+        void loan(const int addedLoan){
+            _loan += addedLoan;
+        }
+
+        int get_debt_amount() const {
+            return _loan * 1.1;
+        }
+
+
+        int make_plush(const int cost){
+            auto plush = Plush(cost);
+                
+            if(0 <= get_wealth_amount() - cost || get_debt_amount() - cost ){
+                _vecPlush.push_back(plush);
+                _loan -= cost;
+                _experience++;
+                if(_loan < 0){
+                    _loan = 0;
+                }
+            }
+            return plush.get_cost();
+        }
+
+        int  get_experience(){
+            return _experience;
+        }
+    private:
+        std::string _name = "Andil";
+        int _loan = 0;
+        std::vector<Plush> _vecPlush {};
+        int _experience = 0;
+ 
+        
+        
+};
